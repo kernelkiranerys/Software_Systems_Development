945fd3153e18525866b02fd2f9e4717a9b66da93
KernelKiraNerys <J.edwards2529@student.leedsbeckett.ac.uk>
2025-11-20 14:03:20 +0000

    added week 9 person class

---
+++ LabWork/Week9/Person.java
@@ -0,0 +1,14 @@
+package Week9;
+
+public class Person {
+    int heightCM;
+    int ageYears;
+    boolean isMale;
+
+    public Person(int heightCM,int ageYears, boolean isMale){
+        this.heightCM = heightCM;
+        this.ageYears = ageYears;
+        this.isMale = isMale;
+    }
+
+}
